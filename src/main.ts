84d3ce9a7078aa5ee6e1e52a215d80c1d3d0a4d5
Felipe Carli <felipe.carli@compasso.com.br>
2022-05-11 15:32:36 -0300

    Commit Inicial

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {cors: true});
+
+  const config = new DocumentBuilder()
+    .setTitle('API para Desafio 1 NodeJS')
+    .setDescription('API CRUD de usuarios e tarefas')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);  
+  await app.listen(8080);
+}
+bootstrap();
